e2b56928909c7e6f417f6089f27ae62d9ab7f990
urpdrum <rm357140@fiap.com.br>
2024-11-28 14:13:48 -0300

    Teste tabela produscts

---
+++ src/main/java/com/fiap/restaurantes/RestaurantesApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.restaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiap/restaurantes/RestaurantesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.restaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
